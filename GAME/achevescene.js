f67bbd582eea411c7d83e2905ab79aad85e68551
GA-Gamesdev <helpme704586@gmail.com>
2026-02-14 21:10:19 -0500

    Update achevescene.js

--- GAME/achevescene.js
+++ GAME/achevescene.js
@@ -1,4 +1,5 @@
-loadSprite("acheve1","https://image2url.com/r2/default/images/1771118824634-2a3b46b9-a73c-4191-9f04-3513f415bd1e.png")
+loadSprite("acheve1","https://image2url.com/r2/default/images/1771121029822-862d7cd8-c808-448f-988f-3569f26fd97f.png")
+loadSprite("lock","https://image2url.com/r2/default/images/1771121393431-b8bd46aa-25fc-4124-bdcc-5fce41da8fcd.png")
 scene("achevements",()=>{
 if(acheve.isUnlocked("f8an9dj")){
 add([
@@ -20,13 +21,14 @@ rect(760,90,{radius:40}),
 pos(10,20)
 ])
     add([
-  sprite("acheve1"),
+  sprite("lock"),
     scale(0.5),
       anchor("center"),
       pos(50,65)
   ])
 }
 })
+
 
 
 
