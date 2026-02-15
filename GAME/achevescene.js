457b60ebcf24175b41fb19040257c45894b17de5
GA-Gamesdev <helpme704586@gmail.com>
2026-02-14 20:20:45 -0500

    Update achevescene.js

--- GAME/achevescene.js
+++ GAME/achevescene.js
@@ -1,18 +1,19 @@
 scene("achevements",()=>{
 if(acheve.isUnlocked("f8an9dj")){
 add([
-rect(800,90,{radius:40}),
+rect(760,90,{radius:40}),
 
 pos(10,0)
 ])
 }else{
 add([
-rect(800,90,{radius:40}),
+rect(760,90,{radius:40}),
 
 pos(10,0)
 ])
 }
 })
+
 
 
 
